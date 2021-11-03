cb57210e5ac7828b4f480ba66a1d54f58405e04f
carter-kilgour <71785488+carter-kilgour@users.noreply.github.com>
2021-11-02 21:32:40 -0400

    commit

---
+++ get-recipes/index.js
@@ -0,0 +1,40 @@
+const MongoClient = require('mongodb').MongoClient;
+const auth = {
+  user: process.env.user,
+  password: process.env.password
+};
+let db = null;
+const loadDB = async () => {
+  if (db) {
+    return db;
+  }
+  const client = await MongoClient.connect(
+    `mongodb://${process.env.user}.documents.azure.com:${
+      process.env.port
+    }/?ssl=true`,
+    {
+      auth: auth
+    }
+  );
+  db = client.db('admin');
+  return db;
+};
+
+module.exports = async function(context) {
+  try {
+    const database = await loadDB();
+    let recipes = await database
+      .collection('Recipes')
+      .find()
+      .toArray();
+    context.res = {
+      body: { items: recipes }
+    };
+  } catch (error) {
+    context.log(`Error code: ${error.code} message: ${error.message}`);
+    context.res = {
+      status: 500,
+      body: { message: 'An error has occured, please try again later' }
+    };
+  }
+};
